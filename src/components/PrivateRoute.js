bc8f29838b55c622875e34442f47eab988338440
riteshk64 <riteshkumar.hb@gmail.com>
2023-08-08 22:14:54 +0530

    Created public and private routes.

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,16 @@
+import { Route,Redirect } from "react-router-dom";
+
+
+const PrivateRoute = ({ children, ...routeProps}) => {
+    const profile = false;
+
+    if(!profile){
+        return <Redirect to="/signin" />
+    }
+
+    return (<Route {...routeProps}>
+        {children} 
+    </Route>
+)};
+
+export default PrivateRoute;
---
+++ src/components/PublicRoute.js
@@ -0,0 +1,16 @@
+import { Route,Redirect } from "react-router-dom";
+
+
+const PublicRoute = ({ children, ...routeProps}) => {
+    const profile = false;
+
+    if(profile){
+        return <Redirect to="/" />
+    }
+
+    return (<Route {...routeProps}>
+        {children} 
+    </Route>
+)};
+
+export default PublicRoute;
