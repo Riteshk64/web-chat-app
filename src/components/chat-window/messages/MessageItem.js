2c180e5bce52f582c465e536c07319779b8a9749
riteshk64 <riteshkumar.hb@gmail.com>
2023-08-16 20:54:40 +0530

    Handle message likes.

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -6,13 +6,14 @@ import PresenceDot from "../../PresenceDot";
 import { Button } from "rsuite";
 import { useCurrentRoom } from "../../../context/current-room.context";
 import { auth } from "../../../misc/firebase";
-import { useHover } from "../../../misc/custom-hooks";
+import { useHover, useMediaQuery } from "../../../misc/custom-hooks";
 import IconBtnControl from "./IconBtnControl";
 
-const MessageItem = ({message,handleAdmin}) => {
-    const {author,createdAt,text} = message;
+const MessageItem = ({message,handleAdmin, handleLike}) => {
+    const {author,createdAt,text, likes, likeCount} = message;
 
     const [selfRef, isHovered] = useHover();
+    const isMobile = useMediaQuery('(max-width: 992px)');
 
     const isAdmin = useCurrentRoom(v => v.isAdmin);
     const admins = useCurrentRoom(v => v.admins);
@@ -21,6 +22,8 @@ const MessageItem = ({message,handleAdmin}) => {
     const isAuthor = auth.currentUser.uid === author.uid;
     const canGrantAdmin = isAdmin && !isAuthor;
 
+    const canShowIcons = isMobile || isHovered;
+    const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
     return ( 
     <li className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-02' : ''}`} ref={selfRef}>
         <div className="d-flex align-items-center font-bolder mb-1">
@@ -34,7 +37,7 @@ const MessageItem = ({message,handleAdmin}) => {
                 }
             </ProfileInfoBtnModal>
             <TimeAgo datetime={createdAt} className="font-normal text-black-45 ml-2"/>
-             <IconBtnControl {...(true ? {color: 'red'} : {})} isVisible iconName="heart" tooltip="Like this message" onClick={() => {}} badgeContent={5} />
+             <IconBtnControl {...(isLiked ? {color: 'red'} : {})} isVisible={canShowIcons} iconName="heart" tooltip="Like this message" onClick={() => handleLike(message.id)} badgeContent={likeCount} />
         </div>
         <div>
             <span className="word-break-all">{text}</span>
