fbfad0648af879093c6f40dec700c1f67b035ad8
riteshk64 <riteshkumar.hb@gmail.com>
2023-08-16 09:26:48 +0530

    Created icon control button component.

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-constant-condition */
 import TimeAgo from "timeago-react";
 import ProfileAvatar from "../../ProfileAvatar";
 import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
@@ -6,6 +7,7 @@ import { Button } from "rsuite";
 import { useCurrentRoom } from "../../../context/current-room.context";
 import { auth } from "../../../misc/firebase";
 import { useHover } from "../../../misc/custom-hooks";
+import IconBtnControl from "./IconBtnControl";
 
 const MessageItem = ({message,handleAdmin}) => {
     const {author,createdAt,text} = message;
@@ -32,6 +34,7 @@ const MessageItem = ({message,handleAdmin}) => {
                 }
             </ProfileInfoBtnModal>
             <TimeAgo datetime={createdAt} className="font-normal text-black-45 ml-2"/>
+             <IconBtnControl {...(true ? {color: 'red'} : {})} isVisible iconName="heart" tooltip="Like this message" onClick={() => {}} badgeContent={5} />
         </div>
         <div>
             <span className="word-break-all">{text}</span>
