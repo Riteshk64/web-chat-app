e08939377304d33eba6b064a60048c56d2b0d582
riteshk64 <riteshkumar.hb@gmail.com>
2023-08-16 21:34:25 +0530

    Handle delete message operation.

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -65,12 +65,38 @@ const Messages = () => {
               return msg; 
         });
         Alert.info(alertMsg, 4000);
-    },[]);
+    }, []);
+
+    const handleDelete = useCallback(async(msgId) => {
+        if(!window.confirm('Delete this message?')){
+            return;
+        }
+        const isLastMessage = messages[messages.length - 1].id === msgId;
+        const updates = {};
+        updates[`/messages/${msgId}`] = null;
+        if(isLastMessage && messages.length > 1){
+            updates[`/rooms/${chatId}/lastMessage`] = {
+                ...messages[messages.length - 2],
+                msgId: messages[messages.length - 2].id
+            }
+        }
+
+        if(isLastMessage && messages.length === 1){
+            updates[`/rooms/${chatId}/lastMessage`] = null;
+        }
+
+        try {
+            await database.ref().update(updates);
+            Alert.info('Message has deleted');
+        } catch (error) {
+            Alert.error(error.message);
+        }
+    }, [chatId,messages]);
 
     return ( 
     <ul className="msg-list custom-scroll">
         {isChatEmpty && <li>No messages yet</li>}
-        {canShowMessages && messages.map((msg) => <MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} handleLike={handleLike} />)}
+        {canShowMessages && messages.map((msg) => <MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} handleLike={handleLike} handleDelete={handleDelete}/>)}
     </ul> 
     );
 }
